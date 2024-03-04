37264b186553bb37c243f4112b917a9a9b24455d
Ritu Rani Mohapatra <78473864+RituRanii@users.noreply.github.com>
2024-03-04 07:47:37 +0000

    conversion and comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,3 +8,16 @@ console.log(booleanIsLoggedIn)
 //1:true; 0:false
 //"":false
 //"ritu":true
+
+// ********************** Operations *****************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.log(2+|-||*|**|/|%);
+let str1 = "hello"
+let str2 = "  ritu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
