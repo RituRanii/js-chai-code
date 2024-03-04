caedbaec9dd5114c3158a33e793bb8a3311c54c6
Ritu Rani Mohapatra <78473864+RituRanii@users.noreply.github.com>
2024-03-04 06:51:13 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 1234
+let accountEmail = "ritu@gmail.com"
+var accountPassword = "1234"
+accountCity = "Cuttack"
+//accountID = 2 //not allowed
+console.log(accountID);
+accountEmail = "acfd@yahoo.com"
+accountPassword="121890"
+accountCity="bbsr"
+console.table([accountID,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+//"use strict"; //treat all JS code as newer version
+//alert("Hello") error dikhega abhi in nodejs 
+let name = "Ritu"
+let age = 23
+let isLoggedIn = false
+//datatype
+// number, bigint, string(""), boolean(true/false), null(type:stand-alone value), undefined(value not assigned), symbol(unique), object
+console.log(typeof null);
