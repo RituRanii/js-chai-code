e30c2878db0d597267357ede84552420b80ad412
Ritu Rani Mohapatra <78473864+RituRanii@users.noreply.github.com>
2024-03-04 07:16:45 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+// let score = "33"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1:true; 0:false
+//"":false
+//"ritu":true
