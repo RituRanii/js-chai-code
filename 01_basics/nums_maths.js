690ee69a2eb79cedb64ec021d11b159127fefee2
Ritu Rani Mohapatra <78473864+RituRanii@users.noreply.github.com>
2024-03-05 15:19:45 +0000

    math and nums

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,10 @@
+const score = 400
+const balance = new Number(100.97);
+console.log(score);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
+const number = 100000
+console.log(number.toLocaleString('en-IN'));
