668268f0f27a36229c70433219f77be21846b2c1
Ritu Rani Mohapatra <78473864+RituRanii@users.noreply.github.com>
2024-03-06 16:35:09 +0000

    api

---
+++ 02_basics/05_API.js
@@ -0,0 +1,26 @@
+//objects de-structure and JSON API into
+
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    Instructor:"ritu"
+}
+const {Instructor}= course
+console.log(Instructor);
+
+// const navbar = ({company}) => {
+//destructuring
+// }
+// navbar(Company = "hitesh")
+
+// {
+//     "name":"ritu",
+//     "coursename": "eng"
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
